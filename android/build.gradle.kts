97c6a5b1d93c17445bcb85db97eb00cb5be22279
Ahmedalaa10204 <ahmedalaa10204@gmail.com>
2026-05-23 16:38:14 +0300

    fix: Update NDK version to 29.0.14033849 in build.gradle.kts files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,21 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            (project.extensions.getByName("android") as com.android.build.gradle.BaseExtension).apply {
+                compileSdkVersion(36)
+                ndkVersion = "29.0.14033849"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
